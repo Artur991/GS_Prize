e56121da799717c52f1b73a5226e80338db74db9
Artur <fox9910@gmail.com>
2023-12-15 01:59:07 +0500

    почти

--- GS_Prize.java
+++ GS_Prize.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class GS_Prize {
     private List<GS> toys;
