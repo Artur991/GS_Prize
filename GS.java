d96581c871a1cd25d15fd709c90b783303670392
Artur <fox9910@gmail.com>
2023-12-15 00:41:43 +0500

    методы возврата  значений

--- GS.java
+++ GS.java
@@ -9,4 +9,27 @@ public class GS {
         this.quantity = quantity;
         this.weight = weight;
     }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
