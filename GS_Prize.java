40afabe8dec002044ffa8cbfa6867d10e7f25a50
Artur <fox9910@gmail.com>
2023-12-15 00:43:46 +0500

    необходимые библиотеки

---
+++ GS_Prize.java
@@ -0,0 +1,10 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GS_Prize {
+    private List<Toy> toys;
+    private List<Toy> prizeToys;
+}
