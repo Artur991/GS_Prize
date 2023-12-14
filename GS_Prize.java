480bf405de2537397972e0268e732d2b1dfc2922
Artur <fox9910@gmail.com>
2023-12-15 00:46:23 +0500

    Добовление  игрушек

--- GS_Prize.java
+++ GS_Prize.java
@@ -7,8 +7,13 @@ import java.util.Random;
 public class GS_Prize {
     private List<GS> toys;
     private List<GS> prizeToys;
-}
+
 public GS_Prize() {
-        toys = new ArrayList<>();
-        prizeToys = new ArrayList<>();
-    }
+    toys = new ArrayList<>();
+    prizeToys = new ArrayList<>();
+}
+public void addToy(int id, String name, int quantity, double weight) {
+    GS toy = new GS(id, name, quantity, weight);
+    toys.add(toy);
+}
+}
